696635c2435e0441bb697e6a8a79eac77fd28380
Mario <144950133+Mario8802@users.noreply.github.com>
2023-11-04 15:04:03 +0100

    Update 07_rounding.py

---
+++ functions/functions/07_rounding.py
@@ -0,0 +1,8 @@
+number = input().split()
+my_lst = []
+for num in number:
+    my_lst.append(round(float(num)))
+print(my_lst)    
+
+
+# print(list(map(lambda roundinglikeaboss: round(float(roundinglikeaboss)), input().split())))
