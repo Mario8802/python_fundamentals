2ef3b1884562e742ae980ce26babe30e58358fcc
Mario <144950133+Mario8802@users.noreply.github.com>
2024-01-13 23:23:21 +0100

    Update 06_string_purenes.py

--- basic_syntax_conditional_statements_and_loops/06_string_purenes.py
+++ basic_syntax_conditional_statements_and_loops/06_string_purenes.py
@@ -7,3 +7,12 @@ for current_string in range(number_of_strings):
         print(f"{pure_or_not_pure_string} is not pure!")
     else:
         print(f"{pure_or_not_pure_string} is pure.")
+
+# n = int(input())
+
+# for _ in range(n):
+#     command = input()
+#     if "," in command or "." in command or "_" in command:
+#         print(f"{command} is not pure!")
+#     else:
+#         print(f"{command} is pure.")
