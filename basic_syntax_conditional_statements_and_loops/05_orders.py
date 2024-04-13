ca411b171ec46a65d170ff9bb1742243ece6023b
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-13 12:45:48 +0200

    Update 05_orders.py

--- basic_syntax_conditional_statements_and_loops/05_orders.py
+++ basic_syntax_conditional_statements_and_loops/05_orders.py
@@ -1,23 +1,3 @@
-# number_of_orders = int(input())
-# total_sum = 0
-# for order in range(number_of_orders):
-
-#     price_per_capsule = float(input())
-#     days = int(input())
-#     capsules_needed_per_day = int(input())
-#     total_price = (days * capsules_needed_per_day) * price_per_capsule
-#     total_sum += total_price
-#     if 0.01 < price_per_capsule > 100.00:
-#         continue
-#     elif 1 > days > 31:
-#         continue
-#     elif 1 > capsules_needed_per_day > 2000:
-#         continue
-#         print(f"The price for the coffee is: ${total_price:.2f}")
-
-# print(f"Total: ${total_sum:.2f}")
-
-
 number_of_orders = int(input())
 total_sum = 0
 
