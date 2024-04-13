897b1dfcb0eb7509a9623290ba227403e96dd22d
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-13 12:46:15 +0200

    Update 01._jenny's_secret_message.py

--- basic_syntax_conditional_statements_and_loops/01._jenny's_secret_message.py
+++ basic_syntax_conditional_statements_and_loops/01._jenny's_secret_message.py
@@ -1,7 +1,9 @@
-name = input()
+def greets_the_user(self):
+    if name == "Johnny":
+        print("Hello, my love!")
+    else:
+        print(f"Hello, {name}!")
 
-if name == "Johnny":
-    print("Hello, my love!")
-else:
-    print(f"Hello, {name}!")
- 
+
+name = input()
+greets_the_user(name)
