a189dd3e794b4c1e099ab1339718e4c29831e6e1
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-15 19:49:17 +0200

    Update print_part_of_the_ascii_table.py

--- data_types_and_variables/exercise/print_part_of_the_ascii_table.py
+++ data_types_and_variables/exercise/print_part_of_the_ascii_table.py
@@ -1,5 +1,14 @@
-start_index = int(input())
-final_index = int(input())
+# start_index = int(input())
+# final_index = int(input())
 
-for number in range(start_index,final_index + 1):
-    print(chr(number), end = ' ')
+# for number in range(start_index,final_index + 1):
+#     print(chr(number), end = ' ')
+
+
+start = int(input())
+end = int(input())
+total_sum_of_chars = ""
+
+for char in range(start, end + 1):
+    total_sum_of_chars += chr(char)
+print(" ".join(total_sum_of_chars))
