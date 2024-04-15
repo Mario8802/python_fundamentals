7cc9ebd100f25fd727134d5d5f97ec3eaf361d64
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-15 19:41:11 +0200

    Update sum_of_chars.py

--- data_types_and_variables/exercise/sum_of_chars.py
+++ data_types_and_variables/exercise/sum_of_chars.py
@@ -1,6 +1,16 @@
-number_of_characters =  int(input())
-total_sum = 0
-for character in range(number_of_characters):
-    current_character = input()
-    total_sum += ord(current_character)
-print(f"The sum equals: {total_sum}")
+# number_of_characters =  int(input())
+# total_sum = 0
+# for character in range(number_of_characters):
+#     current_character = input()
+#     total_sum += ord(current_character)
+# print(f"The sum equals: {total_sum}")
+
+n = int(input())
+total_sum_of_chars = 0
+if 1 > n or n > 20:
+    exit()
+
+for _ in range(n):
+    char = input()
+    total_sum_of_chars += ord(char)
+print(f"The sum equals: {total_sum_of_chars}")
