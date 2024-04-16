6e992c89d1bb3fd5cf470d0b14d2c6a67f44d510
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-16 07:32:29 +0200

    Update 06_next_happy_year.py

--- data_types_and_variables/06_next_happy_year.py
+++ data_types_and_variables/06_next_happy_year.py
@@ -11,6 +11,8 @@ while not happy_year_condition:
 
 print(year)
 
+
+=======================================================================
 year = int(input())
 is_happy_year = False
 
@@ -23,5 +25,33 @@ while not is_happy_year:
     #     set_year.add(str_year[i])
 
     is_happy_year = len(str_year) == len(set_year)
+
+print(year)
+
+========================================================================
+
+year = int(input())
+while len(set(str(year + 1))) != len(str(year + 1)): year += 1
+print(year + 1)
+
+
+========================================================================
+
+
+year = int(input())
+
+while True:
+    year += 1
+    digits = []
+    temp_year = year
+    while temp_year > 0:
+        digit = temp_year % 10
+        if digit in digits:
+            break
+        digits.append(digit)
+        temp_year //= 10
+
+    if len(digits) == len(str(year)):
+        break
 
 print(year)
