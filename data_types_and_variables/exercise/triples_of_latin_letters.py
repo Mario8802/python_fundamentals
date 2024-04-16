fee6a9d3e3f2edbc27b76e0fb75cfc826c4084b4
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-16 22:09:20 +0200

    Update triples_of_latin_letters.py

--- data_types_and_variables/exercise/triples_of_latin_letters.py
+++ data_types_and_variables/exercise/triples_of_latin_letters.py
@@ -4,3 +4,11 @@ for first_symbol in range(number_of_symbols):
     for second_symbol in range(number_of_symbols):
         for third_symbol in range(number_of_symbols):
             print(f"{chr(97 + first_symbol)}{chr(97 + second_symbol)}{chr(97 + third_symbol)}")
+=========================================================
+
+n = int(input())
+
+for first_num in range(97, 97 + n):
+    for second_num in range(97, 97 + n):
+        for third_num in range(97, 97 + n):
+            print(f"{chr(first_num)}{chr(second_num)}{chr(third_num)}")
