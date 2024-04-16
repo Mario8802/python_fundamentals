734ce92eaef57b2e9ec7a1cdfbd0f156e48d2fa7
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-16 07:23:23 +0200

    Update 06_next_happy_year.py

--- data_types_and_variables/06_next_happy_year.py
+++ data_types_and_variables/06_next_happy_year.py
@@ -18,9 +18,9 @@ is_happy_year = False
 while not is_happy_year:
     year += 1
     str_year = str(year)
-    set_year = set()
-    for i in range(len(str_year)):
-        set_year.add(str_year[i])
+    set_year = set(str_year)  # easy way
+    # for i in range(len(str_year)):  #  
+    #     set_year.add(str_year[i])
 
     is_happy_year = len(str_year) == len(set_year)
 
