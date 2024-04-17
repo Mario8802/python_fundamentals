f17139169f847a5a8ff3432ebd3d3d5141afcdfc
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-17 23:23:02 +0200

    Update 08_party_profit.py

--- data_types_and_variables/exercise/08_party_profit.py
+++ data_types_and_variables/exercise/08_party_profit.py
@@ -18,3 +18,33 @@ for current_day in range(1, days + 1):
     coins -= number_of_companions * 2
 coins_per_companions = coins // number_of_companions
 print(f"{number_of_companions} companions received {coins_per_companions} coins each.")
+
+
+import math
+
+companions = int(input())
+days = int(input())
+coins = 0
+
+for day in range(1,days + 1):
+    if day % 15 == 0:
+        companions += 5
+
+    if day % 10 == 0:
+        companions -= 2
+
+    coins += (50 - 2 * companions)
+
+    if day % 3 == 0:
+        coins -= (3 * companions)
+
+    if day % 5 == 0:
+        coins += (20 * companions)
+         
+        if day % 3 == 0:
+            coins -= (2 * companions)
+
+total_sum_coins_per_companion = math.floor(coins / companions)
+
+print(f"{companions} companions received {total_sum_coins_per_companion} coins each.")
+
