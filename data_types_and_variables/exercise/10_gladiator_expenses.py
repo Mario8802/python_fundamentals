3403df565904cba24c169db73c72852e1fe38eb8
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-18 09:33:07 +0200

    Update 10_gladiator_expenses.py

--- data_types_and_variables/exercise/10_gladiator_expenses.py
+++ data_types_and_variables/exercise/10_gladiator_expenses.py
@@ -14,3 +14,28 @@ expenses = total_helmet_broken * helmet_price \
             + total_armor_broken * armor_price
 
 print(f"Gladiator expenses: {expenses:.2f} aureus")
+
+===============================================================================================================================================
+
+
+count_lost_fights = int(input())
+
+helmet_cost = float(input())
+sword_cost = float(input())
+shield_cost = float(input())
+armor_cost = float(input())
+costs = 0
+shield_breaks = 0
+
+for battle in range(1, count_lost_fights + 1):
+    if battle % 2 == 0:
+        costs += helmet_cost
+    if battle % 3 == 0:
+        costs += sword_cost
+    if battle % 6 ==0:
+        costs += shield_cost
+        shield_breaks += 1
+        if shield_breaks % 2 == 0 and not shield_breaks == 0:
+            costs += armor_cost
+
+print(f"Gladiator expenses: {costs:.2f} aureus")
