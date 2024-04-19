a0cc5a0edf99fb9e53d6b97312fe759747c65550
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-19 20:09:47 +0200

    Update 01_invert_values.py

--- lists_basic/lists_basic_exercise/01_invert_values.py
+++ lists_basic/lists_basic_exercise/01_invert_values.py
@@ -1,9 +1,18 @@
-factor = int(input())
-count = int(input())
+# factor = int(input())
+# count = int(input())
 
-my_list = []
+# my_list = []
 
-for number in range(count):
-    if number * factor > 0:
-        my_list.append(number)
-print(my_list)
+# for number in range(count):
+#     if number * factor > 0:
+#         my_list.append(number)
+# print(my_list)
+
+===========================================================
+
+
+numbers = input().split()
+invert_values = []
+for i in numbers:
+    invert_values.append(-int(i))
+print(invert_values)
