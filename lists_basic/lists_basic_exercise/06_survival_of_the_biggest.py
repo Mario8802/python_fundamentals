dabbcb485f6b16b9e5556729029ee96bfdefb90c
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-20 23:17:41 +0200

    Update 06_survival_of_the_biggest.py

--- lists_basic/lists_basic_exercise/06_survival_of_the_biggest.py
+++ lists_basic/lists_basic_exercise/06_survival_of_the_biggest.py
@@ -11,3 +11,19 @@ for _ in range(count_of_numbers_to_remove):
     lists_with_numbers.remove(min(lists_with_numbers))
 
 print(*lists_with_numbers, sep=", ")
+
+
+
+========================================================================
+
+
+nums = [int(x) for x in input().split()]
+n = int(input())
+
+
+for _ in range(n):
+    nums.remove(min(nums))
+
+
+result = ", ".join(map(str, nums))
+print(result)
