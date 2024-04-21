486454f0a7c2459d482313c58bc38e99b410f336
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-21 13:44:04 +0200

    Update bread_factory.py

--- lists_basic/lists_basic_exercise/bread_factory.py
+++ lists_basic/lists_basic_exercise/bread_factory.py
@@ -41,3 +41,58 @@ if not close_condition:
     print("Day completed!")
     print(f"Coins: {coins}")
     print(f"Energy: {energy}")
+
+
+================================================================================
+
+
+working_day_events = input().split("|")
+
+energy = 100
+coins = 100
+gained_energy = 0
+earned_coins = 0
+ingredients = []
+job_done = False
+
+for event in working_day_events:
+    event_details = event.split('-')
+    event_name = event_details[0]
+    number = int(event_details[1])
+
+    if event_name == "rest":
+        if energy + number <= 100:
+            energy += number
+            gained_energy = number  
+            print(f"You gained {gained_energy} energy.")
+            print(f"Current energy: {energy}.")
+        else:
+            gained_energy = 100 - energy  0
+            energy = 100
+            print(f"You gained {gained_energy} energy.")
+            print(f"Current energy: {energy}.")
+    elif event_name == "order":
+        energy -= 30
+        if energy >= 0:
+            coins += number
+            earned_coins += number
+            print(f"You earned {number} coins.")
+        else:
+            energy += 50
+            print("You had to rest!")
+            job_done = True
+            continue  
+    else:
+        if coins - number >= 0:
+            ingredients.append(event_name)
+            coins -= number
+            print(f"You bought {event_name}.")
+        else:
+            print(f"Closed! Cannot afford {event_name}.")
+            job_done = True
+            break
+
+if not job_done:
+    print("Day completed!")
+    print(f"Coins: {coins}")
+    print(f"Energy: {energy}")
