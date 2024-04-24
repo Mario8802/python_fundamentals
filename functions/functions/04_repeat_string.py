e149e5a42a0f11f1d5d7111a1da4b76b8f5babd7
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-24 07:10:47 +0200

    Update 04_repeat_string.py

--- functions/functions/04_repeat_string.py
+++ functions/functions/04_repeat_string.py
@@ -5,3 +5,15 @@ string = input()
 counter = int(input())
 result = repeat_string(string,counter)
 print(result)
+
+
+
+
+def repeat_string(text, num):
+    return text * num
+
+
+string = input()
+n = int(input())
+
+print(repeat_string(string, n))
