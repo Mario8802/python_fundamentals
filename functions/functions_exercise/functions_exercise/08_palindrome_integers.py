99988a15cb98a9913e09b4fd5e844825fc2a27e5
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-25 23:20:34 +0200

    Update 08_palindrome_integers.py

--- functions/functions_exercise/functions_exercise/08_palindrome_integers.py
+++ functions/functions_exercise/functions_exercise/08_palindrome_integers.py
@@ -6,3 +6,18 @@ numbers_as_string = input().split(", ")
 for number_as_string in numbers_as_string:
     print(is_palindrome(number_as_string))
 
+
+============================================================================
+
+
+def palindrome_integers(nums):
+    for num in nums:
+        if num == num[::-1]:
+            print(True)  # Print True if the number is a palindrome
+        else:
+            print(False)  # Print False if the number is not a palindrome
+
+
+sequence_of_numbers = input().split(', ')
+palindrome_integers(sequence_of_numbers)
+
