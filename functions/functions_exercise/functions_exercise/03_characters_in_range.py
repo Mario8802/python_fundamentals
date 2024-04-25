25a068b71b2fea39740a0e67a9f17665f73037cc
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-25 16:21:20 +0200

    Update 03_characters_in_range.py

--- functions/functions_exercise/functions_exercise/03_characters_in_range.py
+++ functions/functions_exercise/functions_exercise/03_characters_in_range.py
@@ -8,3 +8,20 @@ def characters_in_range(start, end):
 start_step = input()
 end_step = input()
 print(characters_in_range(start_step, end_step))
+
+
+
+====================================================
+
+def chars_in_range(start, end):
+    global chars
+    my_list_with_chars = list()
+    for i in range(ord(start) + 1,ord(end)):
+        my_list_with_chars.append(chr(i))
+        chars = (' '.join(my_list_with_chars))
+    return chars
+
+
+char1 = input()
+char2 = input()
+print(chars_in_range(char1, char2))
