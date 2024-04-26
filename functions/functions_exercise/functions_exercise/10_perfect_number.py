cd570f888da64b4513d874a46d159d6831801302
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-26 22:59:26 +0200

    Update 10_perfect_number.py

--- functions/functions_exercise/functions_exercise/10_perfect_number.py
+++ functions/functions_exercise/functions_exercise/10_perfect_number.py
@@ -11,3 +11,20 @@ def is_perfect(some_number: int) -> str:
 number = int(input())
 print(is_perfect(number))
 
+================================================
+
+
+def is_perfect_number(num):
+    divisors_sum = 0
+    for i in range(1, num):
+        if num % i == 0:
+            divisors_sum += i
+    if divisors_sum == num:
+        print("We have a perfect number!")
+    else:
+        print("It's not so perfect.")
+
+
+number = int(input())
+is_perfect_number(number)
+
