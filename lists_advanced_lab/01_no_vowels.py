21de6ec1b040a494d6dc28382798951267505cce
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-27 14:37:31 +0200

    Update 01_no_vowels.py

--- lists_advanced_lab/01_no_vowels.py
+++ lists_advanced_lab/01_no_vowels.py
@@ -2,3 +2,12 @@ text = input()
 vowels = ['a', 'o', 'u', 'e', 'i']
 result = [ch for ch in text if ch.lower() not in vowels]
 print(''.join(result))
+
+=========================================================================
+
+
+my_list_with_vowels = ['a', 'o', 'u', 'e', 'i']
+
+text = input()
+word = [i for i in text if not i.lower() in my_list_with_vowels]
+print("".join(word))
