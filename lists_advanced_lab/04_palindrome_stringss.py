78e3a4f31d0f23c2229c6a44415bbb02436885e7
Mario <144950133+Mario8802@users.noreply.github.com>
2024-04-27 17:04:53 +0200

    Update 04_palindrome_stringss.py

--- lists_advanced_lab/04_palindrome_stringss.py
+++ lists_advanced_lab/04_palindrome_stringss.py
@@ -11,3 +11,35 @@ palindrome_counter = palindrome_list.count(palindrome_word)
 
 print(palindrome_list)
 print(f'Found palindrome {palindrome_counter} times')
+
+===========================================================================
+
+
+word = input().split()
+palindrome = input()
+
+palindromes = [w for w in word if w == w[::-1]]
+
+print(palindromes)
+print(f"Found palindrome {word.count(palindrome)} times")
+
+============================================================================
+
+
+word = input().split()
+palindrome = input()
+my_list_for_printing = []
+my_list = []
+for searcher_word in word:
+    if palindrome == searcher_word:
+        my_list.append(searcher_word)
+    if searcher_word == searcher_word[::-1]:
+        my_list_for_printing.append(searcher_word)
+
+print(my_list_for_printing)
+print(f"Found palindrome {len(my_list)} times")
+
+
+
+
+
