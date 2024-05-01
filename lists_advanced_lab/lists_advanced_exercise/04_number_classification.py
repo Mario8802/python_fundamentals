d00ebfa22453ac8922f0aad0bcef0db76b8ee11a
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-01 20:27:23 +0200

    Update 04_number_classification.py

--- lists_advanced_lab/lists_advanced_exercise/04_number_classification.py
+++ lists_advanced_lab/lists_advanced_exercise/04_number_classification.py
@@ -20,3 +20,34 @@ print(f"Negative: {negative_numbers(numbers)}")
 print(f"Even: {even_numbers(numbers)}")
 print(f"Odd: {odd_numbers(numbers)}")
 
+==============================================================================
+
+
+
+
+def positive(some_nums):
+    pos_nums = [x for x in nums if x >= 0]
+    return f"Positive: {', '.join(map(str,pos_nums))}"
+
+
+def negative(some_nums):
+    neg_nums = [x for x in nums if x < 0]
+    return f"Negative: {', '.join(map(str,neg_nums))}"
+
+
+def even_numbers(some_nums):
+    even_nums = [x for x in nums if x % 2 == 0]
+    return f"Even: {', '.join(map(str,even_nums))}"
+
+
+def odd_numbers(some_nums):
+    odd_nums = [x for x in nums if x % 2 != 0]
+    return f"Odd: {', '.join(map(str, odd_nums))}"
+
+
+nums = [int(x) for x in input().split(', ')]
+
+print(positive(nums))
+print(negative(nums))
+print(even_numbers(nums))
+print(odd_numbers(nums))
