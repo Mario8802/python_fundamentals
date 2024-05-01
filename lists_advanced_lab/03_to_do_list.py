7932f60ee577e7b4879c6cbf49a17105de89ba41
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-01 08:02:35 +0200

    Update 03_to_do_list.py

--- lists_advanced_lab/03_to_do_list.py
+++ lists_advanced_lab/03_to_do_list.py
@@ -14,3 +14,22 @@ def process_todo_notes():
 
 result = process_todo_notes()
 print(result)
+
+
+============================================================
+
+
+to_do_list = [0] * 10
+
+while True:
+    command = input()
+    if command == "End":
+        break
+    tokens = command.split('-')
+    priority = int(tokens[0]) - 1
+    note = tokens[1]
+    to_do_list.pop(priority)
+    to_do_list.insert(priority, note)
+
+result = [el for el in to_do_list if el != 0]
+print(result)
