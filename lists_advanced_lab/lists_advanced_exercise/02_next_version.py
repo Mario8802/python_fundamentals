af5c6c29543086cb2bea602b52fb9f4a0fe802e2
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-01 17:57:05 +0200

    Update 02_next_version.py

--- lists_advanced_lab/lists_advanced_exercise/02_next_version.py
+++ lists_advanced_lab/lists_advanced_exercise/02_next_version.py
@@ -7,3 +7,19 @@ for index in range(len(version) - 1, -1, -1):
         if index - 1 >= 0:
             version[index - 1] += 1
 print(".".join(str(number) for number in version))
+
+========================================================================
+
+version = input().split('.')
+
+n1, n2, n3 = int(version[0]), int(version[1]), int(version[2])
+n3 += 1
+
+if n3 == 10:
+    n3 = 0
+    n2 += 1
+    if n2 == 10:
+        n2 = 0
+        n1 += 1
+
+print(f"{n1}.{n2}.{n3}")
