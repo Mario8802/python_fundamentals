ff9ad4f6a96cbfba170153ddd18550a8c822109d
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-01 12:31:27 +0200

    Update 05_sorting_names.py

--- lists_advanced_lab/05_sorting_names.py
+++ lists_advanced_lab/05_sorting_names.py
@@ -4,3 +4,11 @@ def sort_names():
     return sorted(names_list, key=lambda name: (-len(name), name))
 
 print(sort_names())
+
+
+def sorted_names(some_names):
+    return sorted(some_names, key=lambda x: (-len(x), x))
+
+
+names = input().split(', ')
+print(sorted_names(names))
