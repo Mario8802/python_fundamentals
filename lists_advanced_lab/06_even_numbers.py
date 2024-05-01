70973a5e077a5d74cd1eda4339bb76dc4efc0a68
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-01 12:35:23 +0200

    Update 06_even_numbers.py

--- lists_advanced_lab/06_even_numbers.py
+++ lists_advanced_lab/06_even_numbers.py
@@ -1,3 +1,16 @@
 numbers = list(map(int, input().split(', ')))
 indices = [i for i in range(len(numbers)) if numbers[i] % 2 == 0]
 print(indices)
+
+
+===============================================================
+
+
+numbers = [int(x) for x in input().split(', ')]
+even_indices = []
+
+for i, num in enumerate(numbers):
+    if num % 2 == 0:
+        even_indices.append(i)
+
+print(even_indices)
