4a3d6be89a8e02f0dee693c5cb797b38530f8dd5
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-01 18:04:07 +0200

    Update 03_word_filter.py

--- lists_advanced_lab/lists_advanced_exercise/03_word_filter.py
+++ lists_advanced_lab/lists_advanced_exercise/03_word_filter.py
@@ -2,3 +2,9 @@ words = input().split()
 filtered_words = [word for word in words if len(word) % 2 == 0]
 print("\n".join(filtered_words))
 
+===================================================================
+
+
+word = [w for w in input().split() if len(w) % 2 == 0]
+
+print('\n'.join(word))
