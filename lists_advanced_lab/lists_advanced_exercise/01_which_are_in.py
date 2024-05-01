c8c31b88e2f06631f5ca5388ae8f573601bf1be3
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-01 17:50:48 +0200

    Update 01_which_are_in.py

--- lists_advanced_lab/lists_advanced_exercise/01_which_are_in.py
+++ lists_advanced_lab/lists_advanced_exercise/01_which_are_in.py
@@ -9,4 +9,22 @@ for first_string in first_sequence:
             substrings.append(first_string)
             break
 print(substrings)
-            
+
+
+===========================================================
+
+
+first_string = input().split(', ')
+second_string = input().split(', ')
+# my_string = []
+
+# for i in first_string:
+#     for j in second_string:
+#         if i in j:
+#             my_string.append(i)
+#             break
+# print(my_string)
+
+
+my_string = [i for i in first_string if any(i in j for j in second_string) ]
+print(my_string)
