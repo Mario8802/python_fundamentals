1a34abd288905b2f74841ce94831d0edaa92a1a5
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-04 15:37:20 +0200

    Update 02_party.py

--- objects_and_classes/02_party.py
+++ objects_and_classes/02_party.py
@@ -1,20 +1,40 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+
+
+# class Party:
+#     def __init__(self):
+#         self.people = []
+
+#     def invite(self, person):
+#         self.people.append(person)
+
+#     def name_of_attendees(self):
+#         return ', '.join([person.name for person in self.people])
+
+#     def number_of_guests(self):
+#         return len(self.people)
+=======================================================================
+
 
 
 class Party:
     def __init__(self):
         self.people = []
 
-    def invite(self, person):
-        self.people.append(person)
 
-    def name_of_attendees(self):
-        return ', '.join([person.name for person in self.people])
+party = Party()
+names = input()
+while not names == "End":
+    party.people.append(names)
+    names = input()
+print(f"Going: {', '.join(party.people)}")
+print(f"total: {len(party.people)}")
 
-    def number_of_guests(self):
-        return len(self.people)
+
+========================================================
+
 
 
 party = Party()
