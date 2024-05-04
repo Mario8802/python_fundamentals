0b92e00f2e49e7caa41dc559f7cd2d9b5974c373
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-04 20:27:37 +0200

    Update 03_catalogue.py

--- objects_and_classes/OOP_exercise/03_catalogue.py
+++ objects_and_classes/OOP_exercise/03_catalogue.py
@@ -14,3 +14,24 @@ class Catalogue:
         returning_string = f"Items in the {self.name} catalogue:\n"
         returning_string += "\n".join(sorted(self.products))
         return returning_string
+
+
+==================================================================
+
+
+class Catalogue:
+    products = []
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def add_product(self, product_name: str):
+        Catalogue.products.append(product_name)
+
+    def get_by_letter(self, first_letter: str):
+        return [p for p in Catalogue.products if p[0] == first_letter]
+
+    def __repr__(self):
+        products_sorted = sorted(Catalogue.products)
+        products_str = "\n".join(f"{p}" for p in products_sorted)
+        return f"Items in the {self.name} catalogue:\n{products_str}"
