8dcacee5566f283da5e5de9ce30aeeb6f9f7a7a2
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-04 19:53:41 +0200

    Update 01_storage.py

--- objects_and_classes/OOP_exercise/01_storage.py
+++ objects_and_classes/OOP_exercise/01_storage.py
@@ -11,3 +11,18 @@ class Storage:
 
     def get_products(self):
         return Storage.storage
+
+
+class Storage:
+    storage = []
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def add_product(self, product: str):
+        if self.capacity > 0:  # There is free space
+            self.capacity -= 1
+            Storage.storage.append(product)
+
+    def get_products(self):
+        return Storage.storage
