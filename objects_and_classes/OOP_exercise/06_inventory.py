879c8760b7699b1bde875001cc538ca77f138b56
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-05 11:35:22 +0200

    Update 06_inventory.py

--- objects_and_classes/OOP_exercise/06_inventory.py
+++ objects_and_classes/OOP_exercise/06_inventory.py
@@ -16,3 +16,25 @@ class Inventory:
 
     def __repr__(self):
         return f"Items: {', '.join(self.items)}.\nCapacity left: {self.__capacity}"
+
+
+=================================================================
+
+class Inventory:
+    def __init__(self, capacity: int):
+        self.__capacity = capacity
+        self.items = []
+
+    def add_item(self, item: str):
+        if len(self.items) < self.__capacity:
+            self.items.append(item)
+            return "Item added successfully"
+        else:
+            return "Not enough room in the inventory"
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def __repr__(self):
+        left_capacity = self.__capacity - len(self.items)
+        return f"Items: {', '.join(self.items)}.\nCapacity left: {left_capacity}"
