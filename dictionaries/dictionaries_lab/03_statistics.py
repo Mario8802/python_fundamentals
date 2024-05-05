3e844fe3c642ffe4df770359a761b8de1baa18e3
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-05 15:14:42 +0200

    Update 03_statistics.py

--- dictionaries/dictionaries_lab/03_statistics.py
+++ dictionaries/dictionaries_lab/03_statistics.py
@@ -19,6 +19,31 @@ total_quantity = sum(stock.values())
 
 print('Products in stock:')
 
+
+================================================
+
+
+command = input()
+products = {}
+
+while not command == "statistics":
+    line = command.split(': ')
+    key = line[0]
+    value = int(line[1])
+    if key not in products:
+        products[key] = 0
+    products[key] += value
+    command = input()
+
+
+print(f"Products in stock:")
+for key, value in products.items():
+    print(f"- {key}: {value}")
+print(f"Total Products: {len(products.keys())}")
+print(f"Total Quantity: {sum(products.values())}")
+
+# [print(f"-{key}:{value}") for (key,value) in products]
+
 for product, quantity in stock.items():
     print(f'- {product}: {quantity}')
 
