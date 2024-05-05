44723ddea66cfd52b18ec94a1569e886fbeb831f
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-05 14:42:21 +0200

    Update 02_stock.py

--- dictionaries/dictionaries_lab/02_stock.py
+++ dictionaries/dictionaries_lab/02_stock.py
@@ -15,3 +15,27 @@ for current_product in products_to_search:
         print(f'We have {stock_data[current_product]} of {current_product} left')
     else:
         print(f'Sorry, we don\'t have {current_product}')
+
+
+
+====================================================================
+
+
+
+line_with_products = input().split()
+searching_products = input().split()
+
+my_products_dict = {}
+
+# Populate the dictionary with product quantities
+for i in range(0, len(line_with_products), 2):
+    product = line_with_products[i]
+    quantity = line_with_products[i + 1]
+    my_products_dict[product] = int(quantity)
+
+# Check for each product and print the availability
+for product in searching_products:
+    if product in my_products_dict:
+        print(f"We have {my_products_dict[product]} of {product} left")
+    else:
+        print(f"Sorry, we don't have {product}")
