cc2d254563ff24aed875718973121384ad02490b
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-06 21:53:10 +0200

    Update 05_ascii_values.py

--- dictionaries/dictionaries_exercise/dictionaries_lab/05_ascii_values.py
+++ dictionaries/dictionaries_exercise/dictionaries_lab/05_ascii_values.py
@@ -3,3 +3,8 @@ characters = input().split(', ')
 char_dict = {char: ord(char) for char in characters}
 
 print(char_dict)
+
+
+====================================================
+
+print(dict((char, ord(char)) for char in input().split(', ')))
