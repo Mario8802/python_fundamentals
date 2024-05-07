c8e4213fa340a66b5ba821fd0d152fb3977741eb
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-07 07:02:38 +0200

    Update 03_capitals.py

--- dictionaries/dictionaries_exercise/03_capitals.py
+++ dictionaries/dictionaries_exercise/03_capitals.py
@@ -13,3 +13,13 @@ for country, capital in final_information.items():
 # separated by comma and space ", ", and their corresponding capital cities on the second line
 # (again separated by comma and space ", ").
 # Print each country with their capital on a separate line in the following format: "{country} -> {capital}".
+
+
+countries = input().split(', ')
+cities = input().split(', ')
+
+zip_countries = zip(countries,cities)
+my_dictionary_with_countries = dict(zip_countries)
+
+for k,v in my_dictionary_with_countries.items():
+    print(f"{k} -> {v}")
