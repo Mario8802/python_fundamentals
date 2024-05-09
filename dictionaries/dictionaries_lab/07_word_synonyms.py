1b411a2ecc6245b521876d7fa8255895a22da500
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-09 10:43:06 +0200

    Update 07_word_synonyms.py

--- dictionaries/dictionaries_lab/07_word_synonyms.py
+++ dictionaries/dictionaries_lab/07_word_synonyms.py
@@ -14,3 +14,21 @@ for _ in range(n):
 for word, synonym_list in synonyms.items():
     synonym_string = ', '.join(synonym_list)
     print(f'{word} - {synonym_string}')
+
+
+=====================================================
+
+n = int(input())
+
+my_dict = {}
+
+for _ in range(n):
+    word = input()
+    synonym = input()
+
+    if word not in my_dict:
+        my_dict[word] = []
+    my_dict[word].append(synonym)
+
+for key, value in my_dict.items():
+    print(f"{key} - {', '.join(value)}")
