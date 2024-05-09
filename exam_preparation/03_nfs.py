38584d7a1a2896ba7fda152c0e64b6c6f8190291
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-09 19:07:02 +0200

    Create 03_nfs.py

---
+++ exam_preparation/03_nfs.py
@@ -0,0 +1,51 @@
+def drive_car(car, distance, fuel):
+    if cars[car]["fuel"] < fuel:
+        print("Not enough fuel to make that ride")
+    else:
+        cars[car]["mileage"] += distance
+        cars[car]["fuel"] -= fuel
+        print(f"{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
+        if cars[car]["mileage"] >= 100000:
+            print(f"Time to sell the {car}!")
+            del cars[car]
+
+def refuel_car(car, fuel):
+    if fuel + cars[car]["fuel"] > 75:
+        fuel_added = 75 - cars[car]["fuel"]
+        cars[car]["fuel"] = 75
+    else:
+        fuel_added = fuel
+        cars[car]["fuel"] += fuel
+    print(f"{car} refueled with {fuel_added} liters")
+
+def revert_mileage(car, kilometers):
+    if cars[car]["mileage"] - kilometers < 10000:
+        cars[car]["mileage"] = 10000
+    else:
+        cars[car]["mileage"] -= kilometers
+        print(f"{car} mileage decreased by {kilometers} kilometers")
+
+n = int(input())
+cars = {}
+
+for _ in range(n):
+    car, mileage, fuel = input().split("|")
+    cars[car] = {"mileage": int(mileage), "fuel": int(fuel)}
+
+while True:
+    command = input().split(" : ")
+    if command[0] == "Stop":
+        break
+    action, car = command[0], command[1]
+    if action == "Drive":
+        distance, fuel = int(command[2]), int(command[3])
+        drive_car(car, distance, fuel)
+    elif action == "Refuel":
+        fuel = int(command[2])
+        refuel_car(car, fuel)
+    elif action == "Revert":
+        kilometers = int(command[2])
+        revert_mileage(car, kilometers)
+
+for car, info in cars.items():
+    print(f"{car} -> Mileage: {info['mileage']} kms, Fuel in the tank: {info['fuel']} lt.")
