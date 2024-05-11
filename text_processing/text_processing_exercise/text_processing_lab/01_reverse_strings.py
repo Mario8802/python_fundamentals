b691f24489f34f12e45a20659cde27ef6229aa6c
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-11 20:13:07 +0200

    Update 01_reverse_strings.py

--- text_processing/text_processing_exercise/text_processing_lab/01_reverse_strings.py
+++ text_processing/text_processing_exercise/text_processing_lab/01_reverse_strings.py
@@ -12,4 +12,19 @@ while True:
         break
 
     print(f"{command} = {command[::-1]}")
+===================================================================================================================================================
+class StringReverser:
+    def __init__(self):
+        pass
 
+    def reverse_strings(self):
+        while True:
+            command = input()
+            if command == 'end':
+                break
+
+            print(f"{command} = {command[::-1]}")
+
+if __name__ == "__main__":
+    reverser = StringReverser()
+    reverser.reverse_strings()
