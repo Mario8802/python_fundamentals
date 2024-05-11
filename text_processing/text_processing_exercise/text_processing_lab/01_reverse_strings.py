cd10076cebfe3c96c624f3799495b94f7ac60c07
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-11 20:12:31 +0200

    Update 01_reverse_strings.py

--- text_processing/text_processing_exercise/text_processing_lab/01_reverse_strings.py
+++ text_processing/text_processing_exercise/text_processing_lab/01_reverse_strings.py
@@ -4,3 +4,12 @@ while True:
         break
     else:
         print(f"{command} = {command[::-1]}")
+
+
+while True:
+    command = input()
+    if command == 'end':
+        break
+
+    print(f"{command} = {command[::-1]}")
+
