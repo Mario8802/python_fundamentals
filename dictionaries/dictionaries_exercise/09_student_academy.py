a65e43fead1b6f8e6ab49aca5c647c1e7cb23659
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-11 14:09:56 +0200

    Update 09_student_academy.py

--- dictionaries/dictionaries_exercise/09_student_academy.py
+++ dictionaries/dictionaries_exercise/09_student_academy.py
@@ -18,3 +18,29 @@ for student, grade in final_dictonary.items():
 
     if avg_grade >= 4.50:
         print(f'{student} -> {avg_grade:.2f}')
+
+
+
+==============================================================================
+
+
+
+
+n = int(input())
+student_data = {}
+
+for _ in range(n):
+    student = input()
+    grade = float(input())
+
+    if student not in student_data:
+        student_data[student] = [grade, 1]
+    else:
+        student_data[student][0] += grade
+        student_data[student][1] += 1
+
+for student, data in student_data.items():
+    sum_grades, num_grades = data
+    average_grade = sum_grades / num_grades
+    if average_grade >= 4.50:
+        print(f"{student} -> {average_grade:.2f}")
