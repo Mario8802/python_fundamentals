e9bdc9ff9f2680396ca53dbd3e4f3aecba9a42aa
Mario <144950133+Mario8802@users.noreply.github.com>
2024-05-11 20:12:03 +0200

    Update 02_repeat_strings.py

--- text_processing/text_processing_exercise/text_processing_lab/02_repeat_strings.py
+++ text_processing/text_processing_exercise/text_processing_lab/02_repeat_strings.py
@@ -4,3 +4,15 @@ for el in string:
     new_string += el * len(el)
 
 print(new_string)
+
+=====================================================
+
+words = input().split()
+new_words = []
+
+for word in words:
+    repeated_word = word * len(word)
+    new_words.append(repeated_word)
+
+final_string = ''.join(new_words)
+print(final_string)
