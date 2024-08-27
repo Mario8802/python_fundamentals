608aab86a6fd43a8f2f14a6d60fc8178e86e434d
Mario <144950133+Mario8802@users.noreply.github.com>
2024-08-28 00:55:38 +0200

    Update 01_computer_store.py

--- exam/Fundamentals - Exams/01_programming_fundamentals_mid_exam_retake/01_computer_store.py
+++ exam/Fundamentals - Exams/01_programming_fundamentals_mid_exam_retake/01_computer_store.py
@@ -27,3 +27,39 @@ else:
         print(f"Total price: {discount:.2f}$")
     else:
         print(f"Total price: {(total_price + taxes):.2f}$")
+
+
+
+=========================================================================================================
+
+
+data = input()
+total_sum = 0
+taxes = 0
+
+while data != 'special' and data != 'regular':
+    try:
+        price = float(data)
+        if price <= 0:
+            print("Invalid price!")
+        else:
+            total_sum += price
+            taxes += price * 0.2
+    except ValueError:
+        print("Invalid input!")
+
+    data = input()
+
+if total_sum == 0:
+    print("Invalid order!")
+else:
+    total_price = total_sum + taxes
+    print(f"Congratulations you've just bought a new computer!")
+    print(f"Price without taxes: {total_sum:.2f}$")
+    print(f"Taxes: {taxes:.2f}$")
+    print("-----------")
+    
+    if data == 'special':
+        total_price *= 0.9  # Прилагаме 10% отстъпка
+    
+    print(f"Total price: {total_price:.2f}$")
