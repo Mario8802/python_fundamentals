ae0215e85d13695fa1e9eb061b22a9882e92c18c
Mario <144950133+Mario8802@users.noreply.github.com>
2024-08-28 15:43:04 +0200

    Update 02_the_lift.py

--- exam/Fundamentals - Exams/01_programming_fundamentals_mid_exam_retake/02_the_lift.py
+++ exam/Fundamentals - Exams/01_programming_fundamentals_mid_exam_retake/02_the_lift.py
@@ -23,3 +23,33 @@ elif people > 0:
     print(" ".join(wagons))
 elif empty_places == all_people and people == 0:
     print(" ".join(wagons))
+
+
+
+
+def fill_the_lift(people, lift):
+    max_capacity = 4
+
+    for i in range(len(lift)):
+        available_space = max_capacity - lift[i]
+
+        if people >= available_space:
+            lift[i] += available_space
+            people -= available_space
+        else:
+            lift[i] += people
+            people = 0
+            break
+
+    if people == 0 and any(wagon < max_capacity for wagon in lift):
+        print("The lift has empty spots!")
+    elif people > 0 and all(wagon == max_capacity for wagon in lift):
+        print(f"There isn't enough space! {people} people in a queue!")
+
+    print(" ".join(map(str, lift)))
+
+
+people = int(input())
+lift = list(map(int, input().split()))
+
+fill_the_lift(people, lift)
